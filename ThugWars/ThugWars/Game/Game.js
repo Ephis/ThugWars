36b6ef959dbf835524ea84cfda36f4b3caddf316
Nicolai Reigstad <nicolai887@gmail.com>
2016-06-10 15:49:47 +0200

    Phaser Setup with fight state

---
+++ ThugWars/ThugWars/Game/Game.js
@@ -0,0 +1,7 @@
+﻿var main = main || {};
+
+main.game = new Phaser.Game(800, 600, Phaser.AUTO, '');
+
+main.game.state.add('Fight', fight);
+
+main.game.state.start('Fight');
---
+++ ThugWars/ThugWars/Game/src/state/FightState.js
@@ -0,0 +1,20 @@
+﻿var fight = function(game) {
+    this.game = game;
+}
+
+fight.prototype = {
+    
+    preload: function() {
+        this.game.stage.backgroundColor = '#eee';
+        this.game.scale.pageAlignHorizontally = true;
+        this.game.scale.pageAlignVertically = true;
+    },
+
+    create: function() {
+        
+    },
+
+    update: function() {
+        
+    }
+}
